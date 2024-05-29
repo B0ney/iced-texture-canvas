f2eea7534e0d81be2163115fb78d8ca293e9686b
B0ney <40839054+B0ney@users.noreply.github.com>
2024-05-29 13:45:48 +0100

    prototype

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
+use iced::widget;
+use iced::{Element, Length, Size};
 mod shader;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> iced::Result {
+    iced::program("title", ShaderApp::update, ShaderApp::view).run()
+}
+
+#[derive(Debug)]
+enum Message {}
+struct ShaderApp {
+    program: shader::Bitmap,
+}
+
+impl Default for ShaderApp {
+    fn default() -> Self {
+        Self {
+            program: shader::Bitmap::new(Size::new(500, 500)),
+        }
+    }
+}
+
+impl ShaderApp {
+    fn update(&mut self, msg: Message) {}
+
+    fn view(&self) -> Element<Message> {
+        widget::shader(&self.program)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+    }
 }
